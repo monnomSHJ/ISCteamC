bfcdbd0d6be315cfc16425e50c9e9d5b0569e612
monnomSHJ <monnomshj22@gmail.com>
2024-05-29 21:58:15 +0900

    add files and folder

---
+++ openingScene.js
@@ -0,0 +1,14 @@
+class OpeningScene {
+    static preload() {
+        //여기에 이미지 로드하기 ex. MainMenu.imageName = loadImage()
+    }
+
+    display() {
+        //여기에 디스플레이 설정하기
+        //각 오브젝트 변수에 따라 다르게 디스플레이 할 것!
+    }
+
+    handleClick() {
+        //클릭 시 이벤트 여기에다가!
+    }
+}
--- sketch.js
+++ sketch.js
@@ -2,15 +2,22 @@ let currentScene; //현재 장면
 let day = 1; //현재 일차
 
 let fontNeo;
+let cursorImage;
 
 function preload() {
-  fontNeo = loadFont('assets/neoMo.TTF');
+  fontNeo = loadFont('assets/fonts/neoMo.TTF');
 }
 
 function setup() {
   createCanvas(1280, 720);
+  currentScene = new MainMenu();
 }
 
 function draw() {
   background(220);
+  currentScene.display();
+}
+
+function mouseClicked() {
+  currentScene.handleClick();
 }
