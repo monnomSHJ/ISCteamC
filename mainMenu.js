c6bbbd759a6bd68c2f53e9e587f57e4ccbb6f169
monnomSHJ <monnomshj22@gmail.com>
2024-05-31 15:12:44 +0900

    add images

---
+++ mainMenu.js
@@ -0,0 +1,14 @@
+class MainMenu {
+    static preload() {
+        //여기에 이미지 로드하기 ex. MainMenu.imageName = loadImage()
+    }
+
+    display() {
+        //여기에 디스플레이 설정하기
+        //각 오브젝트 변수에 따라 다르게 디스플레이 할 것!
+    }
+
+    handleClick() {
+        currentScene = openingScene;
+    }
+}
--- sketch.js
+++ sketch.js
@@ -2,20 +2,25 @@ let currentScene; //현재 장면
 let day = 1; //현재 일차
 
 let fontNeo;
-let cursorImage;
+let cursorImage1;
+let cursorImage2;
 
 function preload() {
   fontNeo = loadFont('assets/fonts/neoMo.TTF');
+  cursorImage1 = loadImage('assets/images/mouseCursor1.png');
+  cursorImage2 = loadImage('assets/images/mouseCursor2.png');
 }
 
 function setup() {
   createCanvas(1280, 720);
   currentScene = new MainMenu();
+  noCursor();
 }
 
 function draw() {
   background(220);
   currentScene.display();
+  image(cursorImage1, mouseX, mouseY);
 }
 
 function mouseClicked() {
