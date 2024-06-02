0b59aaf8dc8a5b508da51f248119215c722c47b1
KiseoYang <whaleofcls22@snu.ac.kr>
2024-06-02 16:03:42 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,25 +4,31 @@ let day = 1; //현재 일차
 let fontNeo;
 let cursorImage1;
 let cursorImage2;
+let openingScene;
 
 function preload() {
   fontNeo = loadFont('assets/fonts/neoMo.TTF');
   cursorImage1 = loadImage('assets/images/mouseCursor1.png');
   cursorImage2 = loadImage('assets/images/mouseCursor2.png');
+  OpeningScene.preload();
 }
 
 function setup() {
   createCanvas(1280, 720);
-  currentScene = new MainMenu();
+  //currentScene = new MainMenu();
   noCursor();
+  openingScene = new OpeningScene();
 }
 
 function draw() {
   background(220);
-  currentScene.display();
+  //currentScene.display();
+  openingScene.display();
+  openingScene.updateDisplayedText();
   image(cursorImage1, mouseX, mouseY);
 }
 
 function mouseClicked() {
-  currentScene.handleClick();
+  //currentScene.handleClick();
+  openingScene.handleClick();
 }
