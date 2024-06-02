62b0dd821847072208f40959bed9770465965633
KiseoYang <whaleofcls22@snu.ac.kr>
2024-06-03 01:19:46 +0900

    Update school.js

--- school.js
+++ school.js
@@ -1,14 +1,53 @@
 class School {
+    constructor() {
+        this.images = School.images;
+        this.alpha =0;
+        this.fadeIn= true;
+    }
+
     static preload() {
-        //여기에 이미지 로드하기 ex. MainMenu.imageName = loadImage()
+        // 이미지 미리 로드
+        School.images = [
+            loadImage('assets/images/backgrounds/School1.png'),
+            loadImage('assets/images/backgrounds/School2.png'),
+            loadImage('assets/images/backgrounds/School3.png'),
+            loadImage('assets/images/backgrounds/School4.png'),
+            loadImage('assets/images/backgrounds/SchoolColor.png'),
+
+          ];
+
+       // for (let i = 1; i <= 5; i++) {
+            //this.images.push(loadImage(`images/image${i}.jpg`));
+        
     }
 
     display() {
-        //여기에 디스플레이 설정하기
-        //각 오브젝트 변수에 따라 다르게 디스플레이 할 것!
+        
+        tint(255, this.alpha); // 이미지에 알파 값 적용
+        image(this.images[day-1],0,0,1280,720);
+        noTint();
+            if (this.fadeIn) {
+             this.alpha += 10;
+             if (this.alpha >= 255) {
+               this.alpha = 255;
+               this.fadeIn = false;
+             }
+           }
+         
+        
+        // day 변수의 값에 따라 이미지 표시
+       
+        setTimeout(() => {
+            currentScene = wayToHome;
+        }, 3000)
+
     }
 
-    handleClick() {
-        //클릭 시 이벤트 여기에다가!
+    handleClick(){
+
     }
+
+
+ 
+
 }
