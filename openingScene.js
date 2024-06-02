96baad4004c1b5ac4a55ade8fbe7de42023833bd
KiseoYang <whaleofcls22@snu.ac.kr>
2024-06-02 14:27:36 +0900

    Update openingScene.js

--- openingScene.js
+++ openingScene.js
@@ -1,6 +1,15 @@
 class OpeningScene {
     static preload() {
         //여기에 이미지 로드하기 ex. MainMenu.imageName = loadImage()
+        //아직 오브젝트 포함 흑백 이미지가 업로드되지 않아 이미지명은 추가 수정할 예정
+            this.images = [
+      loadImage('images/backgrounds/.png'),
+      loadImage('images/image2.png'),
+      loadImage('images/image3.png'),
+      loadImage('images/image4.png')
+    ];
+  
+        this.images = this.imageNames.map(img => loadImage(img));
     }
 
     display() {
