560323b97ae24f06b5aaaf4d96d4bd5fbc1a610a
mindalpeng2 <163081924+mindalpeng2@users.noreply.github.com>
2024-06-03 15:28:09 +0900

    homenight 변경
    homenight 변경

--- homeNight.js
+++ homeNight.js
@@ -1,77 +1,95 @@
 class HomeNight {
+
     constructor() {
-      this.fadeInAlpha = 0;
-      this.showBookImage = false;
-      this.image = HomeNight.image;
+        this.image = HomeNight.image;
+        //this.book= NightBook.image
+        this.texts = [
+            '오늘은 @@과 @@을 발견했다.',
+            '오늘은 @@과 @@을 발견했다.',
+            '오늘은 @@과 @@을 발견했다.',
+            '오늘은 @@과 @@을 발견했다.',
+            '오늘은 @@과 @@을 발견했다.',
 
+        ];
+        this.currentTextIndex = day - 1;
+        this.displayedText = "";
+        this.textAnimationSpeed = 5; // 애니메이션 속도
+        this.textAnimationCounter = 0; // 애니메이션 카운터
     }
-  
+
     static preload() {
-      HomeNight.image = loadImage('assets/images/backgrounds/homeNightColor.png');
-      HomeNight.homeNightBook = loadImage('assets/images/objects/homeNightBook.png');
-    }
-  
-    setup() {
-      this.fadeInAlpha = 0;
-      this.showBookImage = false;
-      setTimeout(() => {
-        this.showBookImage = true;
-      }, 1000); // 1초 후 이미지 변경
-    }
-  
-    display() {
-        image(this.image, 0, 0, width, height);
-
-      if (this.showBookImage) {
-        tint(255, this.fadeInAlpha);
-        image(HomeNight.homeNightBook, width / 2 - HomeNight.homeNightBook.width / 2, height / 2 - HomeNight.homeNightBook.height / 2);
-        this.fadeInAlpha += 5; // 페이드 인 속도 조절
-        if (this.fadeInAlpha > 255) {
-          this.fadeInAlpha = 255;
-        }
-      }
-  
-      this.displayDayImage();
-      this.drawSleepButton();
-    }
-  
-    displayDayImage() {
-      let selectedImage;
-      if (day === 1) {
-        selectedImage = WayToSchool.wtsStore;
-      } else if (day === 2) {
-        selectedImage = WayToSchool.wtsFlower;
-      } else if (day === 3) {
-        selectedImage = WayToSchool.wtsCat;
-      } else if (day === 4) {
-        selectedImage = WayToSchool.wtsCycle;
-      }
-  
-      if (selectedImage) {
-        image(selectedImage, width - selectedImage.width / 2 - 20, height / 2 - selectedImage.height / 2);
-      }
-    }
-  
-    drawSleepButton() {
-      fill(255);
-      rect(width - 100, height - 50, 80, 30);
-      fill(0);
-      textSize(16);
-      textAlign(CENTER, CENTER);
-      text('잠자기', width - 60, height - 35);
-    }
-  
-    handleClick() {
-      if (mouseX > width - 100 && mouseX < width - 20 && mouseY > height - 50 && mouseY < height - 20) {
-        if (day < 5) {
-          day++;
-          changePage(homeMorning);
+        if (day==1){
+            HomeNight.image = loadImage('assets/images/backgrounds/homeNightMono.png');
         } else {
-          changePage(endingScene);
+            HomeNight.image = loadImage('assets/images/backgrounds/homeNightColor.png');
+
         }
-      }
+
+       // NightBook.image = loadImage('assets/images/assets/homeNightBook.png');
+
     }
-  }
+
+    display() { 
+        if (day == 1){
+            image(this.image, 0, 0, width, height);
+
+        } else {
+            let tintValue = day*51; // day 값에 따라 0에서 255로 매핑
+            // 흑백 효과를 위해 RGB 값을 동일하게 설정
+            tint(tintValue, tintValue, tintValue, 255);    
+            image(this.image, 0, 0, width, height);
+        }
+
+        noTint(); // 다음 이미지에 영향을 주지 않도록 tint 해제
+
+        fill(0);
+        rect(0, height - 120, 1280, 120);
+        rect(0, 0, 1280, 120);
+
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        fill(255);
+        text(this.displayedText, 640, height - 70);
+        this.updateDisplayedText(); // 텍스트 한글자씩 나오는 함수
+
+        if (this.displayedText.length === this.texts[this.currentTextIndex].length) {
+            this.drawSleepButton(); // 모든 텍스트가 다 나오면 버튼을 그리기
+        }
+    }
+
+    updateDisplayedText() {
+        if (this.textAnimationCounter < this.textAnimationSpeed) {
+            this.textAnimationCounter++;
+        } else {
+            if (this.displayedText.length < this.texts[this.currentTextIndex].length) {
+                this.displayedText = this.texts[this.currentTextIndex].substring(0, this.displayedText.length + 1);
+            }
+            this.textAnimationCounter = 0;
+        }
+    }
+
+    drawNightBook() {
+        //this.image()
+    }
+
+    drawSleepButton() {
+        fill(255);
+        rect(width - 100, height - 50, 80, 30);
+        textSize(16);
+        fill(0)
+        textAlign(CENTER, CENTER);
+        text('잠자기', width - 60, height - 35);
+        fill(255);
+    }
+
+    handleClick() {
+        if (this.displayedText.length === this.texts[this.currentTextIndex].length &&
+            mouseX > width - 100 && mouseX < width - 20 && mouseY > height - 50 && mouseY < height - 20) {
+            changePage(homeMorning);
+        }
+    }
+}
+
   
 
 /*
