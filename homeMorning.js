0d82338ecee1b77d4db12d6b0e2aac746cbcbb9a
mindalpeng2 <163081924+mindalpeng2@users.noreply.github.com>
2024-06-03 14:16:26 +0900

    전역변수 day 변경 및 morning 수정

--- homeMorning.js
+++ homeMorning.js
@@ -2,11 +2,16 @@ class HomeMorning {
     constructor() {
         this.image = HomeMorning.image;
         this.texts = [
-            '오늘은 월요일이다. 학교에 가자.',
+            '오늘은 월요일이다. 너무 힘들군.',
             '오늘은 화요일이다. 학교에 가자.',
+            '오늘은 수요일이다. 이틀만 버티면 된다!.',
+            '오늘은 목요일이다. 내일은 불금이다!',
+            '오늘은 금요일이다. 이번 주는 뭔가 평소랑 다른 것 같다.'
         ];
-        this.currentTextIndex = 1;
-        this.displayedText = this.texts[this.currentTextIndex];
+        this.currentTextIndex =day;
+        this.displayedText = "";
+        this.textAnimationSpeed = 5; // 애니메이션 속도
+        this.textAnimationCounter = 0; // 애니메이션 카운터
     }
 
     static preload() {
@@ -23,8 +28,21 @@ class HomeMorning {
         textAlign(CENTER);
         fill(255);
         text(this.displayedText, 640, height - 60);
+        this.updateDisplayedText(); //텍스트 한글자씩 나오는 함수
     }
 
+    updateDisplayedText() {
+        if (this.textAnimationCounter < this.textAnimationSpeed) {
+          this.textAnimationCounter++;
+        } else {
+          if (this.displayedText.length < this.texts[this.currentTextIndex].length) {
+            this.displayedText = this.texts[this.currentTextIndex].substring(0, this.displayedText.length + 1);
+          }
+          this.textAnimationCounter = 0;
+        }
+        } //텍스트 순차적으로 나타나기 
+  
+  
     handleClick() {
         changePage(wayToSchool);
     }
